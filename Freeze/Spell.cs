864f4e4d3c15caad51b5e1643f9071d95044856e
Kishimoto <wyatt.woodley@outlook.com>
2023-03-09 19:28:18 -0500

    Update

--- Freeze/Spell.cs
+++ Freeze/Spell.cs
@@ -3,43 +3,41 @@ using UnityEngine;
 using Extensions;
 using System.Collections;
 
-namespace Freeze; 
+namespace Freeze;
 public class Spell : SpellCastCharge {
     public float minimumHandVelocity = 1.50f;
     public float creatureFreezeTime = 10.0f;
     public float creatureFreezeColorThreshold = 0.30f;
     public float itemFreezeTime = 12.5f;
-    public float range = 100.0f;
-    public float radius = 0.30f;
     public override void UpdateCaster() {
         base.UpdateCaster();
         if (!spellCaster.isFiring) return;
         if (Physics.CapsuleCast(spellCaster.magic.position,
-                                spellCaster.magic.forward * range,
-                                radius,
+                                spellCaster.magic.forward * 100.0f,
+                                0.75f,
                                 spellCaster.magic.forward, 
                                 out var hit)) {
             if (hit.collider.GetComponentInParent<Creature>() is Creature { isPlayer: false } creature) {
-                if (spellCaster.ragdollHand.HandVelocityDirection(Vector3.forward) <= minimumHandVelocity) return;
+                if (spellCaster.ragdollHand.HandMovementDirection(Vector3.forward) < minimumHandVelocity) return;
                 GameManager.local.StartCoroutine(FreezeCreature(creature));
             }
             if (hit.collider.GetComponentInParent<Item>() is Item item) {
-                if (spellCaster.ragdollHand.HandVelocityDirection(Vector3.forward) <= minimumHandVelocity) return;
+                if (spellCaster.ragdollHand.HandMovementDirection(Vector3.forward) < minimumHandVelocity) return;
                 GameManager.local.StartCoroutine(FreezeItem(item));
             }
         }
     }
-    IEnumerator FreezeCreature(Creature creature) {
+    private IEnumerator FreezeCreature(Creature creature) {
         creature?.ragdoll?.SetState(Ragdoll.State.Frozen);
-        creature?.brain.Stop();
+        creature?.brain?.Stop();
         creature?.SetColor(Color.Lerp(creature.GetColor(Creature.ColorModifier.Skin), Color.blue, creatureFreezeColorThreshold),
                           Creature.ColorModifier.Skin,
                           true);
         yield return new WaitForSeconds(creatureFreezeTime);
-        creature?.ragdoll?.SetState(creature.ragdoll.state);
+        creature?.ragdoll?.SetState(Ragdoll.State.Destabilized);
         creature?.LoadBrain();
     }
-    IEnumerator FreezeItem(Item item) {
+    private IEnumerator FreezeItem(Item item) {
         item.rb.isKinematic = true;
         item.rb.useGravity = false;
         yield return new WaitForSeconds(itemFreezeTime);
